efbb59e4b9d379a29029bcae9b310872ced5e4f5
xnnixa <154546393+xnnixa@users.noreply.github.com>
2024-02-21 14:23:51 +0200

    attributes added homework1

--- homework1/src/main/java/com/example/ConstantValues.java
+++ homework1/src/main/java/com/example/ConstantValues.java
@@ -1,7 +1,17 @@
 package com.example;
 
 public class ConstantValues {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
+
+    public static final String NO_NAME = "No name";
+    public static final String NO_TITLE= "No title";
+    public static final String NO_BIRTHDATE = "Not available";
+    public static final String INVALID_BIRTHDAY = "Invalid birthday!";
+    public static final String INCORRECT_CHECKMARK = "Incorrect check mark!";
+    public static final int MIN_ID = 1;
+    public static final int MAX_ID = 100;
+    public static final double MIN_CREDITS = 0.0;
+    public static final double MAX_CREDITS = 300.0;
+    public static final double BACHELOR_CREDITS = 180.0;
+    public static final double MASTER_CREDITS = 120.0;
+    
 }
---
+++ homework1/src/main/java/com/example/Student.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class Student {
+    
+    private String firstName = "No name";
+    private String lastName = "No name";
+    private int id;
+    private double bachelorCredits; 
+    private double masterCredits;
+    private String titleOfMastersThesis = "No title";
+    private String titleOfBachelorThesis = "No title";
+    private int startYear;
+    private int graduationYear;
+    private String birthDate;
+
+}
