57028e7781e669715185a830ac2b9dd0e972e132
xnnixa <154546393+xnnixa@users.noreply.github.com>
2024-04-03 19:30:54 +0300

    addstudentcourse(s) methods  50%

--- homework3/src/main/java/dev/m3s/programming2/homework3/Degree.java
+++ homework3/src/main/java/dev/m3s/programming2/homework3/Degree.java
@@ -1,129 +1,36 @@
 package dev.m3s.programming2.homework3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Degree {
 
     //ATTRIBUTES
     
     private static final int MAX_COURSES = 50;
-    private int count = 0;
     private String degreeTitle = ConstantValues.NO_TITLE;
     private String titleOfThesis = ConstantValues.NO_TITLE;
-    private StudentCourse[] myCourses = new StudentCourse[MAX_COURSES];
-
-    //CONSTRUCTORS
-
-    public StudentCourse[] getCourses() {
-        return myCourses;
-        
-    }
+    List<StudentCourse> myCourses = new ArrayList<>();
 
     //METHODS
 
-    public void addStudentCourses(StudentCourse[] courses) {
-        if (courses != null) {
-            for (StudentCourse course : courses) {
-                addStudentCourse(course);
-            }
-        }  
+    public List<StudentCourse> getCourses() {
+        return myCourses;
+    }
+
+    public void addStudentCourses(List<StudentCourse> courses) {
+        if (courses != null && courses.size() < MAX_COURSES) {
+            addStudentCourse();
+            //keep count
+        }
     }
 
     public boolean addStudentCourse(StudentCourse course) {
-        if (course != null && count < MAX_COURSES) {
-            myCourses[count] = course;
-            count++;
+        if (course != null) {
+            //lisää kurssi
             return true;
         }
         return false;
     }
-
-    public int getCount() {
-        return count;
-    }
-
-    public String getDegreeTitle() {
-        return degreeTitle;
-    }
-
-    public void setDegreeTitle(String degreeTitle) {
-        if (degreeTitle != null && !degreeTitle.isEmpty()) {
-            this.degreeTitle = degreeTitle;
-        }
-    }
-
-    public String getTitleOfThesis() {
-        return titleOfThesis;
-    }
-
-    public void setTitleOfThesis(String titleOfThesis) {
-        if (titleOfThesis != null && !titleOfThesis.isEmpty()) {
-            this.titleOfThesis = titleOfThesis;
-        }
-    }
-
-    public double getCreditsByBase(Character base) {
-        double baseCredits = 0.00;
-        for (StudentCourse course : myCourses) {
-            if (course != null && course.getCourse().getCourseBase().equals(base) && course.isPassed()) {
-                baseCredits += course.getCourse().getCredits();
-            }
-        }
-        return baseCredits;
-    }
-
-    public double getCreditsByType(final int courseType) {
-        double typeCredits = 0.00;
-        for (StudentCourse course : myCourses) {
-            if (course != null && course.getCourse().getCourseType() == courseType && course.isPassed()) {
-                typeCredits += course.getCourse().getCredits();
-            }
-        }
-        return typeCredits;
-    }
-
-    public double getCredits() {
-        double totalCredits = 0.00;
-        for (StudentCourse course : myCourses) {
-            if (course != null && course.isPassed()) {
-                totalCredits += course.getCourse().getCredits();
-            }
-        }
-        return totalCredits;
-    }
-
-    private boolean isCourseCompleted(StudentCourse c) {
-        return c != null && c.isPassed();
-    }
-
-    public void printCourses() {
-        for (StudentCourse course : myCourses) {
-            if (course != null) {
-                System.out.println(course);
-            }
-        }
-    }
-
-    public String toString() {
-        StringBuilder str = new StringBuilder();
-        str.append("Degree [Title: \"").append(degreeTitle).append("\"")
-           .append(" (courses: ").append(myCourses != null ? count : 0).append(")").append("\n")
-           .append("Thesis title: \"").append(titleOfThesis).append("\"\n");
-    
-        int count = 1;
-        for (StudentCourse studentCourse : myCourses) {
-            if (studentCourse != null) {
-                Course course = studentCourse.getCourse();
-                str.append(count++).append(". ")
-                   .append(course.toString())
-
-                .append(" Year: ").append(studentCourse.getYear()).append(", ")
-                   .append("Grade: ").append(studentCourse.getGradeNum()).append("]\n");
-            }
-        }
-        str.append("]");
-        System.out.println("sysout: \n" + str);
-        
-        return str.toString();
-    }
-
 
 }
